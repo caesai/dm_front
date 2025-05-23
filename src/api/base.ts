42a627f947ffbe4bb4c87fb2ccdcaf7f8976d980
Vasily Kharlamov <caesai@yandex.ru>
2025-05-23 13:54:27 +0300

    Deploy Test

--- src/api/base.ts
+++ src/api/base.ts
@@ -1,6 +1,6 @@
 export const BASE_URL = import.meta.env.PROD
     ? 'https://devsoko.ru/api/v1'
-    : 'https://192.168.0.41:8090/api/v1';
+    : 'https://localhost:8090/api/v1';
 
 // export const BASE_URL = import.meta.env.PROD
 //     ? 'https://backend.dreamteam.fm/api/v1'
