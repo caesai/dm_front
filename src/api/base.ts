d5660c7b879c558a350f924c6352656be8d9aec6
Vasily Kharlamov <caesai@yandex.ru>
2025-06-23 14:27:12 +0300

    Development Mode

--- src/api/base.ts
+++ src/api/base.ts
@@ -1,5 +1,5 @@
 export const BASE_URL = import.meta.env.PROD
-    ? 'https://backend.dreamteam.fm/api/v1' : import.meta.env.DEV ? 'https://devsoko.ru/api/v1'
+    ?  import.meta.env.MODE === 'development' ? 'https://devsoko.ru/api/v1' : 'https://backend.dreamteam.fm/api/v1'
     : 'https://localhost:8090/api/v1';
 
 // export const BASE_URL = 'https://backend.dreamteam.fm/api/v1'
