1163c347196993b597768b9cb43b284933e772ac
Vasily Kharlamov <caesai@yandex.ru>
2025-06-23 15:26:39 +0300

    Vite Config Development Mode Test

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import {resolve} from 'path'
 // const BASE_URL = process.env.NODE_ENV == 'production' ? '/dm_front/' : '/';
 // https://vitejs.dev/config/
 export default defineConfig(({mode}) => ({
-        base: mode === 'development' ? '/' : '/dm_front/',
+        base: mode !== 'development' ? '/' : '/dm_front/',
         plugins: [
             // Allows using React dev server along with building a React application with Vite.
             // https://npmjs.com/package/@vitejs/plugin-react-swc
