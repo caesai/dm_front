1f20c223d7b59a11d87de8230b50a6492101cb82
Vasily Kharlamov <caesai@yandex.ru>
2025-06-25 18:01:20 +0300

    New Restaurant

--- src/components/RestaurantPreview/RestrauntPreview.tsx
+++ src/components/RestaurantPreview/RestrauntPreview.tsx
@@ -1,17 +1,17 @@
 import css from './RestrauntPreview.module.css';
 import classNames from 'classnames';
-import { RestaurantBadge } from '@/components/RestaurantPreview/RestaurantBadge/RestaurantBadge.tsx';
-import { Swiper } from 'swiper/react';
-import { FreeMode } from 'swiper/modules';
+import {RestaurantBadge} from '@/components/RestaurantPreview/RestaurantBadge/RestaurantBadge.tsx';
+import {Swiper} from 'swiper/react';
+import {FreeMode} from 'swiper/modules';
 import './swipergap.css';
 import 'swiper/css/bundle';
 import 'swiper/css/free-mode';
-import { SwiperSlide } from 'swiper/react';
-import { RestaurantBadgePhoto } from '@/components/RestaurantPreview/RestaurantBadgePhoto/RestaurantBadgePhoto.tsx';
-import { InfoTag } from '@/components/InfoTag/InfoTag.tsx';
-import { Link } from 'react-router-dom';
-import { FC } from 'react';
-import { IRestaurant } from '@/types/restaurant.ts';
+import {SwiperSlide} from 'swiper/react';
+import {RestaurantBadgePhoto} from '@/components/RestaurantPreview/RestaurantBadgePhoto/RestaurantBadgePhoto.tsx';
+import {InfoTag} from '@/components/InfoTag/InfoTag.tsx';
+import {Link} from 'react-router-dom';
+import {FC} from 'react';
+import {IRestaurant} from '@/types/restaurant.ts';
 import {
     getCurrentTimeShort,
     getCurrentWeekdayShort,
@@ -19,65 +19,74 @@ import {
 } from '@/utils.ts';
 import {useAtom} from "jotai/index";
 import {userAtom} from "@/atoms/userAtom.ts";
+import BBQNEW from '/img/BBQNEW.png';
 
 interface IProps {
     restaurant: IRestaurant;
 }
 
-export const RestaurantPreview: FC<IProps> = ({ restaurant }) => {
+export const RestaurantPreview: FC<IProps> = ({restaurant}) => {
     const [user] = useAtom(userAtom);
+    const wantToBeFirst = () => {
+        //
+    }
     return (
-        <Link className={css.restaurant} to={`/restaurant/${restaurant.id}`}>
+        <Link className={css.restaurant} to={restaurant.id === 11 ? '' : `/restaurant/${restaurant.id}`}>
             <div
                 className={classNames(css.imaged, css.bgImage)}
                 style={{
-                    backgroundImage: `url(${restaurant.thumbnail_photo})`,
+                    backgroundImage: `url(${restaurant.id === 11 ? BBQNEW : restaurant.thumbnail_photo})`,
                 }}
             >
 
-                {user?.username && ['martyad','w0esofwit','egormk','burovburov', 'Sushkazzlo', 'iliathoughts'].includes(user?.username) && restaurant.title === 'Self Edge Japanese' && (
+                {user?.username && ['martyad', 'w0esofwit', 'egormk', 'burovburov', 'Sushkazzlo', 'iliathoughts'].includes(user?.username) && restaurant.title === 'Self Edge Japanese' && (
                     <span className={css.discount}>Скидка 10%</span>
                 )}
-                <div className={css.floatingBadges}>
-                    <Swiper
-                        slidesPerView="auto"
-                        modules={[FreeMode]}
-                        freeMode={true}
-                        spaceBetween={8}
-                        slidesOffsetBefore={15}
-                        slidesOffsetAfter={15}
-                    >
-                        <SwiperSlide
-                            className={css.swiperSlide}
-                            style={{ width: '130px' }}
+                {restaurant.id !== 11 && (
+                    <div className={css.floatingBadges}>
+                        <Swiper
+                            slidesPerView="auto"
+                            modules={[FreeMode]}
+                            freeMode={true}
+                            spaceBetween={8}
+                            slidesOffsetBefore={15}
+                            slidesOffsetAfter={15}
                         >
-                            <RestaurantBadge logo={restaurant.logo_url} />
-                        </SwiperSlide>
-                        {restaurant.photo_cards.map((card) => (
                             <SwiperSlide
                                 className={css.swiperSlide}
-                                style={{ width: '130px' }}
-                                key={`card-${card.id}`}
+                                style={{width: '130px'}}
                             >
-                                <RestaurantBadgePhoto url={card.url} />
+                                <RestaurantBadge logo={restaurant.logo_url}/>
                             </SwiperSlide>
-                        ))}
-                    </Swiper>
-                </div>
-                <div className={css.imagedBottom}>
-                    <div
-                        className={classNames(css.chefPhoto, css.bgImage)}
-                        style={{
-                            backgroundImage: `url(${restaurant.brand_chef.photo_url})`,
-                        }}
-                    ></div>
-                    <div className={css.chefInfo}>
-                        <span className={css.chefTitle}>Бренд-шеф</span>
-                        <span className={css.chefName}>
-                            {restaurant.brand_chef.name}
-                        </span>
+                            {restaurant.photo_cards.map((card) => (
+                                <SwiperSlide
+                                    className={css.swiperSlide}
+                                    style={{width: '130px'}}
+                                    key={`card-${card.id}`}
+                                >
+                                    <RestaurantBadgePhoto url={card.url}/>
+                                </SwiperSlide>
+                            ))}
+                        </Swiper>
                     </div>
-                </div>
+                )}
+
+                {restaurant.id !== 11 && (
+                    <div className={css.imagedBottom}>
+                        <div
+                            className={classNames(css.chefPhoto, css.bgImage)}
+                            style={{
+                                backgroundImage: `url(${restaurant.brand_chef.photo_url})`,
+                            }}
+                        ></div>
+                        <div className={css.chefInfo}>
+                            <span className={css.chefTitle}>Бренд-шеф</span>
+                            <span className={css.chefName}>
+                                {restaurant.brand_chef.name}
+                            </span>
+                        </div>
+                    </div>
+                )}
             </div>
             <div className={css.resInfo}>
                 <div className={css.resTitleWrapper}>
@@ -86,14 +95,18 @@ export const RestaurantPreview: FC<IProps> = ({ restaurant }) => {
                     <span className={css.resSlogan}>{restaurant.address}</span>
                 </div>
                 <div className={css.tags}>
-                    <InfoTag
-                        text={getRestaurantStatus(
-                            restaurant.worktime,
-                            getCurrentWeekdayShort(),
-                            getCurrentTimeShort()
-                        )}
-                    />
-                    <InfoTag text={`Ср. чек ${restaurant.avg_cheque}₽`} />
+                    {restaurant.id === 11 ? <span onClick={wantToBeFirst} className={css.resFirst}>Хочу побывать первым</span> : (
+                        <>
+                            <InfoTag
+                                text={getRestaurantStatus(
+                                    restaurant.worktime,
+                                    getCurrentWeekdayShort(),
+                                    getCurrentTimeShort()
+                                )}
+                            />
+                            <InfoTag text={`Ср. чек ${restaurant.avg_cheque}₽`}/>
+                        </>
+                    )}
                 </div>
             </div>
         </Link>
