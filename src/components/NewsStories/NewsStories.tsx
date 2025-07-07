e8f02a20bd04fe36447135d3835068195b54660b
Vasily Kharlamov <caesai@yandex.ru>
2025-07-07 17:27:59 +0300

    Stories List Fix

--- src/components/NewsStories/NewsStories.tsx
+++ src/components/NewsStories/NewsStories.tsx
@@ -1,6 +1,6 @@
 import { NewsStoriesElement } from '@/components/NewsStories/NewsStoriesElement/NewsStoriesElement.tsx';
 
-// import css from './NewsStories.module.css';
+import css from './NewsStories.module.css';
 import { FreeMode } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import {useState} from "react";
@@ -17,12 +17,13 @@ export const NewsStories = () => {
     return (
         <>
             <StoriesContainer onClose={closeStory} activeIndex={activeIndex} />
+            <div>
             <Swiper
                 slidesPerView="auto"
                 modules={[FreeMode]}
                 freeMode={true}
                 spaceBetween={0}
-                // wrapperClass={css.newsSlider}
+                wrapperClass={css.newsSlider}
             >
                 <SwiperSlide style={{ width: '100px', marginRight: 10 }}>
                     <NewsStoriesElement onClick={openStory} index={0} />
@@ -47,6 +48,7 @@ export const NewsStories = () => {
                 */}
                 <SwiperSlide></SwiperSlide>
             </Swiper>
+            </div>
         </>
     );
 };
