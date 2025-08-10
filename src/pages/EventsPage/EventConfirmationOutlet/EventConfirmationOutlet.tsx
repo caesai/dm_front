0a9c133b2161bcd99bf735bfdfc1cf97f2a3ffe1
Vasily Kharlamov <caesai@yandex.ru>
2025-08-10 15:11:36 +0300

    Event Info Page Changes

--- src/pages/EventsPage/EventConfirmationOutlet/EventConfirmationOutlet.tsx
+++ src/pages/EventsPage/EventConfirmationOutlet/EventConfirmationOutlet.tsx
@@ -149,7 +149,11 @@ export const EventConfirmationOutlet = () => {
                             {bookingInfo.event_date?.tickets_left}
                         </span>
                     </div>
-
+                    <div className={css.event_params_col}>
+                        <div className={css.roundedText}>
+                            <span>✓ 100% предоплата</span>
+                        </div>
+                    </div>
                 </div>
                 {/*<div className={css.event_params_row}>*/}
                 {/*    <div className={css.event_params_col}>*/}
@@ -171,10 +175,6 @@ export const EventConfirmationOutlet = () => {
                 {/*        </span>*/}
                 {/*    </div>*/}
                 {/*</div>*/}
-
-                <div className={css.roundedText}>
-                    <span>✓ 100% предоплата</span>
-                </div>
                 <div className={css.personsContainer}>
                     <span className={css.personsContainer__title}>
                         Количество мест
