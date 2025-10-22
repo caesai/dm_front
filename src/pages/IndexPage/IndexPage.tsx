cc75f277945ed0bd5c6177246c46e540e4cb5c0f
Vasily Kharlamov <caesai@yandex.ru>
2025-10-22 13:32:16 +0300

    Preferences Progress Style Fix

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -1,25 +1,25 @@
-import {FC, useEffect, useMemo, useState} from 'react';
+import { FC, useEffect, useMemo, useState } from 'react';
 import css from './IndexPage.module.css';
 
-import {Page} from '@/components/Page.tsx';
-import {Header} from '@/components/Header/Header.tsx';
-import {OptionsNavigation} from '@/components/OptionsNavigation/OptionsNavigation.tsx';
-import {RestaurantPreview} from '@/components/RestaurantPreview/RestrauntPreview.tsx';
-import {BookingReminder} from '@/components/BookingReminder/BookingReminder.tsx';
-import {useAtom} from 'jotai';
+import { Page } from '@/components/Page.tsx';
+import { Header } from '@/components/Header/Header.tsx';
+import { OptionsNavigation } from '@/components/OptionsNavigation/OptionsNavigation.tsx';
+import { RestaurantPreview } from '@/components/RestaurantPreview/RestrauntPreview.tsx';
+import { BookingReminder } from '@/components/BookingReminder/BookingReminder.tsx';
+import { useAtom } from 'jotai';
 import {
     currentCityAtom,
     setCurrentCityAtom,
 } from '@/atoms/currentCityAtom.ts';
-import {cityListAtom, ICity} from '@/atoms/cityListAtom.ts';
-import {IConfirmationType} from '@/components/ConfirmationSelect/ConfirmationSelect.types.ts';
-import {CitySelect} from '@/components/CitySelect/CitySelect.tsx';
-import {IBookingInfo, IRestaurant} from '@/types/restaurant.ts';
-import {restaurantsListAtom} from '@/atoms/restaurantsListAtom.ts';
-import {APIGetCurrentBookings} from '@/api/restaurants.ts';
+import { cityListAtom, ICity } from '@/atoms/cityListAtom.ts';
+import { IConfirmationType } from '@/components/ConfirmationSelect/ConfirmationSelect.types.ts';
+import { CitySelect } from '@/components/CitySelect/CitySelect.tsx';
+import { IBookingInfo, IRestaurant } from '@/types/restaurant.ts';
+import { restaurantsListAtom } from '@/atoms/restaurantsListAtom.ts';
+import { APIGetCurrentBookings } from '@/api/restaurants.ts';
 import { authAtom, userAtom } from '@/atoms/userAtom.ts';
-import {PlaceholderBlock} from '@/components/PlaceholderBlock/PlaceholderBlock.tsx';
-import {Stories} from "@/components/Stories/Stories.tsx";
+import { PlaceholderBlock } from '@/components/PlaceholderBlock/PlaceholderBlock.tsx';
+import { Stories } from '@/components/Stories/Stories.tsx';
 // import {DEV_MODE} from "@/api/base.ts";
 import { BottomButtonWrapper } from '@/components/BottomButtonWrapper/BottomButtonWrapper.tsx';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
@@ -43,7 +43,7 @@ export const IndexPage: FC = () => {
     const [, setCurrentCityA] = useAtom(setCurrentCityAtom);
     const [cityListA] = useAtom(cityListAtom);
     const [cityListConfirm] = useState<IConfirmationType[]>(
-        cityListA.map((v: ICity) => transformToConfirmationFormat(v))
+        cityListA.map((v: ICity) => transformToConfirmationFormat(v)),
     );
     const [restaurantsList, setRestaurantsList] = useState<IRestaurant[]>([]);
 
@@ -51,7 +51,7 @@ export const IndexPage: FC = () => {
         cityListConfirm.find((v) => v.id == currentCityA) ?? {
             id: 'moscow',
             text: 'Москва',
-        }
+        },
     );
     const [restaurants] = useAtom(restaurantsListAtom);
     const [auth] = useAtom(authAtom);
@@ -78,25 +78,24 @@ export const IndexPage: FC = () => {
         // APIGetTickets(auth.access_token)
         //     .then((res) => setTickets(res.data))
         //     .finally(() => setEventsLoading(false));
-        Promise.all([APIGetCurrentBookings(auth.access_token),APIGetTickets(auth.access_token)])
+        Promise.all([APIGetCurrentBookings(auth.access_token), APIGetTickets(auth.access_token)])
             .then((responses) => {
                 // console.log('responses: ', responses);
                 // @ts-expect-error
-                const events: IBookingInfo[] = responses[1].data.map((event) =>
-                {
-                    return {
-                        id: event.id,
-                        booking_type: 'event',
-                        booking_date: moment(event.date_start).format('YYYY-MM-DD'),
-                        time: moment(event.date_start).format('HH:mm'),
-                        restaurant: event.restaurant,
-                        tags: null,
-                        duration: 0,
-                        guests_count: event.guest_count,
-                        children_count: 0,
-                        event_title: event.event_title,
-                    }
-                }
+                const events: IBookingInfo[] = responses[1].data.map((event) => {
+                        return {
+                            id: event.id,
+                            booking_type: 'event',
+                            booking_date: moment(event.date_start).format('YYYY-MM-DD'),
+                            time: moment(event.date_start).format('HH:mm'),
+                            restaurant: event.restaurant,
+                            tags: null,
+                            duration: 0,
+                            guests_count: event.guest_count,
+                            children_count: 0,
+                            event_title: event.event_title,
+                        };
+                    },
                 );
                 const bookings = [...events, ...responses[0].data.currentBookings];
                 setCurrentBookings(bookings);
@@ -105,8 +104,8 @@ export const IndexPage: FC = () => {
                 setCurrentBookingsLoading(false);
             });
         APIGetSuperEventHasAccess(auth.access_token).then((response) => {
-                setHasSuperEventAccess(response.data);
-        })
+            setHasSuperEventAccess(response.data);
+        });
     }, []);
 
     useEffect(() => {
@@ -114,28 +113,28 @@ export const IndexPage: FC = () => {
             cityListConfirm.find((v) => v.id == currentCityA) ?? {
                 id: 'moscow',
                 text: 'Москва',
-            }
+            },
         );
     }, [cityListA]);
 
     useEffect(() => {
-        let result: IRestaurant[] =[];
-        let movableValue=null;
+        let result: IRestaurant[] = [];
+        let movableValue = null;
 
-        restaurants.map((e)=>{
-            if(e.id !== 11){
+        restaurants.map((e) => {
+            if (e.id !== 11) {
                 result.push(e);
-            }else if(e.id === 11){
-                movableValue=e;
+            } else if (e.id === 11) {
+                movableValue = e;
             }
-        })
+        });
 
-        if(movableValue!==null){
+        if (movableValue !== null) {
             result.unshift(movableValue);
         }
 
         setRestaurantsList(
-            result.filter((v) => v.city.name_english == currentCityA)
+            result.filter((v) => v.city.name_english == currentCityA),
         );
     }, [currentCityA, cityListA]);
 
@@ -151,37 +150,37 @@ export const IndexPage: FC = () => {
     }, [isBanquet]);
 
     useEffect(() => {
-        const preferencesStatus = JSON.parse(localStorage.getItem("PREFERENCES_STATUS") as string)
+        const preferencesStatus = JSON.parse(localStorage.getItem('PREFERENCES_STATUS') as string);
 
-        if (!user?.license_agreement || !user.complete_onboarding || !user?.phone_number) return
+        if (!user?.license_agreement || !user.complete_onboarding || !user?.phone_number) return;
 
         if (!preferencesStatus) {
-            localStorage.setItem("PREFERENCES_STATUS", JSON.stringify({ visit_number: 1 }))
+            localStorage.setItem('PREFERENCES_STATUS', JSON.stringify({ visit_number: 1 }));
             return;
         }
 
         const { visit_number } = preferencesStatus;
 
         if (visit_number === 1) {
-            localStorage.setItem("PREFERENCES_STATUS", JSON.stringify({ visit_number: 2 }))
+            localStorage.setItem('PREFERENCES_STATUS', JSON.stringify({ visit_number: 2 }));
             return;
         }
 
         if (visit_number === 2) {
-            localStorage.setItem("PREFERENCES_STATUS", JSON.stringify({ visit_number: 3, preferences_sent: false }))
-            navigate('/preferences/1')
-            return
+            localStorage.setItem('PREFERENCES_STATUS', JSON.stringify({ visit_number: 3, preferences_sent: false }));
+            navigate('/preferences/1');
+            return;
         }
 
         // Reset preferences counter for testing purposes (delete before RELEASE)
         if (visit_number === 3) {
             localStorage.setItem(
                 'PREFERENCES_STATUS',
-                JSON.stringify({ visit_number: 1 })
+                JSON.stringify({ visit_number: 1 }),
             );
             return;
         }
-        }, [navigate, user?.license_agreement, user?.complete_onboarding, user?.phone_number]);
+    }, [navigate, user?.license_agreement, user?.complete_onboarding, user?.phone_number]);
 
     const updateCurrentCity = (city: IConfirmationType) => {
         setCurrentCityS(city);
@@ -190,46 +189,47 @@ export const IndexPage: FC = () => {
 
     const cityOptions = useMemo(
         () => cityListConfirm.filter(v => v.id !== currentCityS.id),
-        [cityListConfirm, currentCityS.id]
+        [cityListConfirm, currentCityS.id],
     );
 
     const restaurantListed = (currentCityA === 'spb') ? [{
-        "id": 12,
-        "title": "Self Edge Chinois",
-        "slogan": "Современная Азия с акцентом на Китай и культовый raw bar",
-        "address": "Санкт-Перербург, ул. Добролюбова, 11",
-        "logo_url": "",
-        "thumbnail_photo": newres,
-        "avg_cheque": 3000,
-        "about_text": "",
-        "about_dishes": "Европейская",
-        "about_kitchen": "Американская",
-        "about_features": "",
-        "phone_number": "",
-        "address_lonlng": "",
-        "address_station": "",
-        "address_station_color": "",
-        "city": {
-            "id": 2,
-            "name": "Санкт-Петербург",
-            "name_english": "spb",
-            "name_dative": "Санкт-Петербурге"
+        'id': 12,
+        'title': 'Self Edge Chinois',
+        'slogan': 'Современная Азия с акцентом на Китай и культовый raw bar',
+        'address': 'Санкт-Перербург, ул. Добролюбова, 11',
+        'logo_url': '',
+        'thumbnail_photo': newres,
+        'avg_cheque': 3000,
+        'about_text': '',
+        'about_dishes': 'Европейская',
+        'about_kitchen': 'Американская',
+        'about_features': '',
+        'phone_number': '',
+        'address_lonlng': '',
+        'address_station': '',
+        'address_station_color': '',
+        'city': {
+            'id': 2,
+            'name': 'Санкт-Петербург',
+            'name_english': 'spb',
+            'name_dative': 'Санкт-Петербурге',
         },
-        "gallery": [],
-        "brand_chef": {},
-        "worktime": [],
-        "menu": [],
-        "menu_imgs": [],
-        "socials": [],
-        "photo_cards": []
-    },...restaurantsList] : restaurantsList;
+        'gallery': [],
+        'brand_chef': {},
+        'worktime': [],
+        'menu': [],
+        'menu_imgs': [],
+        'socials': [],
+        'photo_cards': [],
+    }, ...restaurantsList] : restaurantsList;
 
     // @ts-ignore
     return (
         <Page back={false}>
             <div className={css.pageContainer}>
-                <Header/>
-                <Stories token={auth?.access_token} cityId={cityListA.find(item => item.name_english === currentCityS.id)?.id} />
+                <Header />
+                <Stories token={auth?.access_token}
+                         cityId={cityListA.find(item => item.name_english === currentCityS.id)?.id} />
                 <div style={{ marginRight: 15 }}>
                     <CitySelect
                         options={cityOptions}
@@ -238,7 +238,7 @@ export const IndexPage: FC = () => {
                     />
                 </div>
                 {currentBookingsLoading ? (
-                    <div style={{marginRight: '15px'}}>
+                    <div style={{ marginRight: '15px' }}>
                         <PlaceholderBlock
                             width={'100%'}
                             height={'108px'}
@@ -248,7 +248,7 @@ export const IndexPage: FC = () => {
                 ) : (
                     currentBookings
                         .filter((book) => {
-                            return new Date(new Date().setUTCHours(0, 0, 0, 0)).getTime() <= new Date(book.booking_date).getTime()
+                            return new Date(new Date().setUTCHours(0, 0, 0, 0)).getTime() <= new Date(book.booking_date).getTime();
                         })
                         .map((book) => (
                             <BookingReminder
@@ -266,13 +266,14 @@ export const IndexPage: FC = () => {
                         ))
                 )}
                 {hasSuperEventAccess && (
-                    <div style={{ marginRight: 15, height: 85}}>
+                    <div style={{ marginRight: 15, height: 85 }}>
                         <Link to={'/events/super'}>
-                            <img src={superevent} style={{ maxWidth: '100%', width: '100%', borderRadius: 16 }} alt={''} />
+                            <img src={superevent} style={{ maxWidth: '100%', width: '100%', borderRadius: 16 }}
+                                 alt={''} />
                         </Link>
                     </div>
                 )}
-                <OptionsNavigation/>
+                <OptionsNavigation />
 
 
                 <div className={css.restaurants}>
@@ -286,7 +287,7 @@ export const IndexPage: FC = () => {
                     ))}
                 </div>
             </div>
-            {currentCityA !== 'ekb' && <BottomButtonWrapper onClick={() => navigate('/booking/')}/>}
+            {currentCityA !== 'ekb' && <BottomButtonWrapper onClick={() => navigate('/booking/')} />}
             <Toast message={toastMessage} showClose={toastShow} />
         </Page>
     );
