06e54f1a5a047143b14859e7776831916c845269
Vasily Kharlamov <caesai@yandex.ru>
2025-10-29 17:34:16 +0300

    Certificates Hide For Prod Env

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,25 +1,27 @@
-import {Page} from '@/components/Page.tsx';
+import { Page } from '@/components/Page.tsx';
 import css from './ProfilePage.module.css';
-import {RoundedButton} from '@/components/RoundedButton/RoundedButton.tsx';
-import {BackIcon} from '@/components/Icons/BackIcon.tsx';
-import {Link, useNavigate,
+import { RoundedButton } from '@/components/RoundedButton/RoundedButton.tsx';
+import { BackIcon } from '@/components/Icons/BackIcon.tsx';
+import {
+    Link, useNavigate,
     // useSearchParams
 } from 'react-router-dom';
-import {TicketsIcon} from '@/components/Icons/TicketsIcon.tsx';
-import {BookIcon} from '@/components/Icons/BookIcon.tsx';
+import { TicketsIcon } from '@/components/Icons/TicketsIcon.tsx';
+import { BookIcon } from '@/components/Icons/BookIcon.tsx';
 // import {PenIcon} from '@/components/Icons/PenIcon.tsx';
-import {UserProfileIcon} from '@/components/Icons/UserProfileIcon.tsx';
+import { UserProfileIcon } from '@/components/Icons/UserProfileIcon.tsx';
 // import { SupportIcon } from '@/components/Icons/SupportIcon.tsx';
 // import { QRCodeIcon } from '@/components/Icons/QRCodeIcon.tsx';
 // import {FeedbackPopup} from '@/pages/ProfilePage/FeedbackPopup/FeedbackPopup.tsx';
 // import {useState} from 'react';
-import {useAtom} from 'jotai';
-import {backButtonAtom} from '@/atoms/backButtonAtom.ts';
+import { useAtom } from 'jotai';
+import { backButtonAtom } from '@/atoms/backButtonAtom.ts';
 import {
     // reviewAtom,
-    userAtom
+    userAtom,
 } from '@/atoms/userAtom.ts';
 import { WalletIcon } from '@/components/Icons/WalletIcon.tsx';
+import { DEV_MODE } from '@/api/base.ts';
 // import {DEV_MODE} from "@/api/base.ts";
 
 export const ProfilePage = () => {
@@ -58,17 +60,17 @@ export const ProfilePage = () => {
                     </div>
                     <div className={css.navLinks}>
                         <Link to={'/myBookings'} className={css.navLink}>
-                            <BookIcon size={24} color={'black'}/>
+                            <BookIcon size={24} color={'black'} />
                             <span className={css.navLinkTitle}>
                                 Мои бронирования
                             </span>
                         </Link>
-                        {tg_id && [5753349682, 217690245, 291146366, 940813721, 1225265717, 1145014952, 5362638149, 551243345, 701368624, 1090746420, 596483540, 1050003812, 542527667, 483425133, 451194888, 1020365281, 7077186349, 229667270, 257329939, 1094749437, 201790418, 79219030954, 706889029, 1357403642, 475197315, 586628247, 244816672, 353624620, 115555014, 153495524, 1283802964, 84327932, 163811519, 7160315434, 118832541, 189652327].includes(tg_id)  &&  (
+                        {tg_id && [5753349682, 217690245, 291146366, 940813721, 1225265717, 1145014952, 5362638149, 551243345, 701368624, 1090746420, 596483540, 1050003812, 542527667, 483425133, 451194888, 1020365281, 7077186349, 229667270, 257329939, 1094749437, 201790418, 79219030954, 706889029, 1357403642, 475197315, 586628247, 244816672, 353624620, 115555014, 153495524, 1283802964, 84327932, 163811519, 7160315434, 118832541, 189652327].includes(tg_id) && (
                             <span
                                 className={css.navLink}
                                 onClick={() => navigate('/tickets')}
                             >
-                                    <TicketsIcon size={24} color={'black'}/>
+                                    <TicketsIcon size={24} color={'black'} />
                                     <span className={css.navLinkTitle}>Мои билеты</span>
                                 </span>
                         )}
@@ -84,20 +86,22 @@ export const ProfilePage = () => {
                         {/*    </span>*/}
                         {/*) : null}*/}
                         <Link to={'/me'} className={css.navLink}>
-                            <UserProfileIcon size={24} color={'black'}/>
+                            <UserProfileIcon size={24} color={'black'} />
                             <span className={css.navLinkTitle}>
                                 Личные данные
                             </span>
                         </Link>
-                        <Link to={'/certificates/my'} className={css.navLink}>
-                            <WalletIcon size={24} color={'black'}/>
-                            <span className={css.navLinkTitle}>
-                                Подарочные сертификаты
-                            </span>
-                        </Link>
+                        {DEV_MODE && (
+                            <Link to={'/certificates/my'} className={css.navLink}>
+                                <WalletIcon size={24} color={'black'} />
+                                <span className={css.navLinkTitle}>
+                                    Подарочные сертификаты
+                                </span>
+                            </Link>
+                        )}
                         {user?.administrator?.is_active ? (
                             <Link to={'/scanner'} className={css.navLink}>
-                                <UserProfileIcon size={24} color={'black'}/>
+                                <UserProfileIcon size={24} color={'black'} />
                                 <span className={css.navLinkTitle}>
                                     Сканер билетов
                                 </span>
