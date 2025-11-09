ee3ea338c8f6e7f8e44fb1285eef3b71c29c3576
kurudoqqe <vadim.firedog@gmail.com>
2025-11-09 03:30:46 +0300

    change stories design

--- src/components/Stories/StoriesBlocksSwiper/StoriesBlock.tsx
+++ src/components/Stories/StoriesBlocksSwiper/StoriesBlock.tsx
@@ -1,23 +1,29 @@
 import css from './StoriesBlock.module.css';
-import React from "react";
+import React from 'react';
 import classNames from 'classnames';
 
 interface StoriesBlockProps {
     onClick: (index: number) => void;
     index: number;
     thumbnail: string;
+    name: string;
     isSeen?: boolean;
 }
 
-export const StoriesBlock: React.FC<StoriesBlockProps> = ({ onClick, thumbnail, index, isSeen }) => {
+export const StoriesBlock: React.FC<StoriesBlockProps> = ({ onClick, thumbnail, name, index, isSeen }) => {
     const handleClick = () => {
         onClick(index);
-    }
+    };
     return (
-        <div className={classNames(css.storyBlock, !isSeen ? css.isClicked : null)} onClick={handleClick}>
-            <div className={css.storyBlockImage}>
-                <img src={thumbnail} alt="" />
+        <section className={css.storyContainer}>
+            <div className={classNames(css.storyBorder, isSeen ? css.clicked : null)}>
+                <div className={classNames(css.storyBlock, !isSeen ? css.active : null)} onClick={handleClick}>
+                    <div className={css.storyBlockImage}>
+                        <img src={thumbnail} alt="" />
+                    </div>
+                </div>
             </div>
-        </div>
+            <span>{name}</span>
+        </section>
     );
 };
