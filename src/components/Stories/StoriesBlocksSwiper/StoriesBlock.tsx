5b26cc4801e360848fd3e0d374a6310fdace4c04
kurudoqqe <vadim.firedog@gmail.com>
2025-11-10 03:29:35 +0300

    delete stories red border

--- src/components/Stories/StoriesBlocksSwiper/StoriesBlock.tsx
+++ src/components/Stories/StoriesBlocksSwiper/StoriesBlock.tsx
@@ -1,23 +1,21 @@
 import css from './StoriesBlock.module.css';
 import React from 'react';
-import classNames from 'classnames';
 
 interface StoriesBlockProps {
     onClick: (index: number) => void;
     index: number;
     thumbnail: string;
     name: string;
-    isSeen?: boolean;
 }
 
-export const StoriesBlock: React.FC<StoriesBlockProps> = ({ onClick, thumbnail, name, index, isSeen }) => {
+export const StoriesBlock: React.FC<StoriesBlockProps> = ({ onClick, thumbnail, name, index }) => {
     const handleClick = () => {
         onClick(index);
     };
     return (
         <section className={css.storyContainer}>
-            <div className={classNames(css.storyBorder, isSeen ? css.clicked : null)}>
-                <div className={classNames(css.storyBlock, !isSeen ? css.active : null)} onClick={handleClick}>
+            <div className={css.storyBorder}>
+                <div className={css.storyBlock} onClick={handleClick}>
                     <div className={css.storyBlockImage}>
                         <img src={thumbnail} alt="" />
                     </div>
