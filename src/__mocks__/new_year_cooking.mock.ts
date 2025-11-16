a7c1a63e456ff63bd43c9071bc84c0a172ce7e59
kurudoqqe <vadim.firedog@gmail.com>
2025-11-17 01:05:27 +0300

    change new year cooking img to yandexcloud url

--- src/__mocks__/new_year_cooking.mock.ts
+++ src/__mocks__/new_year_cooking.mock.ts
@@ -1,6 +1,4 @@
-import NewYearCookingImage from "../../public/img/NewYearCooking.png"
-
 export const NewYearCookingData = {
     description: "Оформите предзаказ новогодних блюд до 28 декабря и получите готовый стол!",
-    image: NewYearCookingImage,
+    image: "https://storage.yandexcloud.net/bottec-dreamteam/07838462279344c4a775c6e57b74c125.png",
 }
