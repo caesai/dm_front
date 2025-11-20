42488211cc4354c13f63ff557bc12f32f3170fda
Vasily Kharlamov <caesai@yandex.ru>
2025-11-20 19:15:54 +0300

    Certificates Selector Test

--- src/__tests__/certificates/certificates.test.tsx
+++ src/__tests__/certificates/certificates.test.tsx
@@ -1,7 +1,26 @@
-// import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
-// import { CertificatesCreateOnlinePage } from '@/pages/CertificatesCreatePage/stages/CertificatesCreateOnlinePage.tsx';
+import { Provider, WritableAtom } from 'jotai';
+import { useHydrateAtoms } from 'jotai/utils'
+import { CertificatesSelector, CertificatesSelectorProps } from '@/components/CertificatesSelector/CertificatesSelector.tsx';
+import { certificatesListAtom } from '@/atoms/certificatesListAtom.ts';
+import { certificatesListMock } from '@/__mocks__/certificates.mock.ts';
+import React, { PropsWithChildren } from 'react';
 
+export type AnyWritableAtom = WritableAtom<unknown, never[], unknown>
+interface InitialProps extends PropsWithChildren {
+    initialValues: Array<readonly [AnyWritableAtom, unknown]>
+}
+const HydrateAtoms: React.FC<InitialProps> = ({ initialValues, children }) => {
+    useHydrateAtoms(initialValues as Array<readonly [AnyWritableAtom, never]>)
+    return children
+}
+
+const TestProvider: React.FC<InitialProps> = ({ initialValues, children }) => (
+    <Provider>
+        <HydrateAtoms initialValues={initialValues}>{children}</HydrateAtoms>
+    </Provider>
+)
 // Mock the react-router-dom module to control navigation
 const mockedUsedNavigate = jest.fn();
 jest.mock('react-router-dom', () => ({
@@ -9,86 +28,40 @@ jest.mock('react-router-dom', () => ({
     useNavigate: () => mockedUsedNavigate,
 }));
 
-// Create a spy function we can reference in the test
-// export const mockNameChangeHandler = jest.fn();
-// export const mockComplimentChangeHandler = jest.fn();
-//
-// jest.mock('@/components/TextInput/TextInput.tsx', () => ({
-//     // Use a render prop pattern in the mock to capture the change handler
-//     TextInput: (props: any) => (
-//         <input
-//             {...props}
-//             data-testid={`input-${props.placeholder}`}
-//             // We attach the parent's actual handler to a manual function here
-//             onChange={(e) => {
-//                 props.onChange(e.target.value);
-//             }}
-//         />
-//     ),
-// }));
+describe('CertificatesSelector', () => {
+    const mockSetCertificateId = jest.fn();
+    // Вспомогательная функция для рендера компонента
+    const renderComponent = (props: Partial<CertificatesSelectorProps> = {}) => {
+        const initialProps: CertificatesSelectorProps = {
+            setCertificateId: mockSetCertificateId,
+            isOpened: true,
+            selectedCertificateId: null,
+        };
 
-describe('CertificatesCreateOnlinePage', () => {
+        return render(
+            // Используем Provider и передаем наши специальные моки через initialValues
+            <TestProvider initialValues={[[certificatesListAtom, certificatesListMock]]}>
+                <CertificatesSelector {...initialProps} {...props} />
+            </TestProvider>
+        );
+    };
+
+    beforeEach(() => {
+        // Очистка мок-функции перед каждым тестом
+        jest.clearAllMocks();
+    });
+
     afterEach(() => {
         jest.clearAllMocks();
     });
 
-    test('renders correctly with initial state', () => {
-        // render(<CertificatesCreateOnlinePage />);
-        //
-        // // Check for initial placeholder text in the display area
-        // expect(screen.getByText('Добавьте приятных слов к подарку')).toBeInTheDocument();
-        // expect(screen.getByText('Имя')).toBeInTheDocument();
-        // expect(screen.getByText('Номинал')).toBeInTheDocument(); // Initial nominal should be empty or a specific placeholder
-        //
-        // // Check for input fields and button
-        // expect(screen.getByPlaceholderText('Имя получателя')).toBeInTheDocument();
-        // expect(screen.getByPlaceholderText('Ваше поздравление')).toBeInTheDocument();
-        // expect(screen.getByRole('button', { name: /далее/i })).toBeInTheDocument();
-    });
+    test('при переходе на страницу бронирования выбирать сертификат из списка', async () => {
+        // Рендерим с изначально выбранным сертификатом ID 'random-id' и закрытым состоянием
+        renderComponent({ isOpened: true, selectedCertificateId: 'SUGNS0AG' });
 
-    test('changes button positioning class on input focus and blur', async () => {
-        // render(<CertificatesCreateOnlinePage />);
-        // const nameInput = screen.getByPlaceholderText('Имя получателя');
-        // const buttonContainer = screen.getByTestId('button-container'); // You'll need to add a data-testid to the button container div in the component
-        //
-        // // Focus the input
-        // fireEvent.focus(nameInput);
-        // await waitFor(() => {
-        //     expect(buttonContainer).toHaveClass('relativeBottom');
-        // });
-        //
-        // // Blur the input
-        // fireEvent.blur(nameInput);
-        // await waitFor(() => {
-        //     expect(buttonContainer).not.toHaveClass('relativeBottom');
-        // });
-    });
-    //
-    // test('enforces input length limits', async () => {
-    //     render(<CertificatesCreateOnlinePage />);
-    //
-    //     const nameInput = screen.getByPlaceholderText('Имя получателя') as HTMLInputElement;
-    //     const complimentInput = screen.getByPlaceholderText('Ваше поздравление') as HTMLInputElement;
-    //
-    //     // --- Test name input length limit (max 15 chars) ---
-    //     const longName = 'A very long name that should be truncated';
-    //     const expectedNameDisplayValue = longName.substring(0, 15);
-    //
-    //     fireEvent.change(nameInput, { target: { value: longName } });
-    //
-    //     // Use findByText to wait for the truncated text to appear in the DOM
-    //     const displayedName = await screen.findByText(expectedNameDisplayValue);
-    //     expect(displayedName).toBeInTheDocument();
-    //
-    //
-    //     // --- Test compliment input length limit (max 30 chars) ---
-    //     const longCompliment = 'A very very very long compliment that should be truncated';
-    //     const expectedComplimentDisplayValue = longCompliment.substring(0, 30);
-    //
-    //     fireEvent.change(complimentInput, { target: { value: longCompliment } });
-    //
-    //     // Use findByText to wait for the truncated text to appear in the DOM
-    //     const displayedCompliment = await screen.findByText(expectedComplimentDisplayValue);
-    //     expect(displayedCompliment).toBeInTheDocument();
-    // });
+        // Ожидаем, что useEffect вызовет setCertificateId с правильным ID
+        await waitFor(() => {
+            expect(mockSetCertificateId).toHaveBeenCalledWith('SUGNS0AG');
+        });
+    })
 });
