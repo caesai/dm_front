24b0f0a99dbf4a4464c09032ade82627d95c0005
kurudoqqe <vadim.firedog@gmail.com>
2025-11-30 18:34:10 +0300

    connect api to getting dishes list

--- src/pages/GastronomyPage/stages/GastronomyChooseDishesPage.tsx
+++ src/pages/GastronomyPage/stages/GastronomyChooseDishesPage.tsx
@@ -1,31 +1,25 @@
-import React from 'react';
-import { mockGastronomyListData } from '@/__mocks__/gastronomy.mock.ts';
+import React, { useEffect, useState } from 'react';
 import css from '@/pages/GastronomyPage/GastronomyPage.module.css';
 import { DishCard } from '@/components/DishCard/DishCard.tsx';
 import { useGastronomyCart } from '@/hooks/useGastronomyCart.ts';
 import { FloatingCartButton } from '@/components/FloatingCartButton/FloatingCartButton.tsx';
 import { useNavigate, useParams } from 'react-router-dom';
 import { IDish } from '@/types/gastronomy.types.ts';
+import { APIGetGastronomyDishes } from '@/api/gastronomy.api.ts';
+import { useAtom } from 'jotai';
+import { authAtom } from '@/atoms/userAtom.ts';
 
 export const GastronomyChooseDishesPage: React.FC = () => {
+    const [auth] = useAtom(authAtom)
     const navigate = useNavigate();
     const { res_id } = useParams();
     const { cart, addToCart, removeFromCart, getItemQuantity } = useGastronomyCart();
+    
+    const [dishesList, setDishesList] = useState<IDish[]>([]);
 
     const handleCartClick = () => {
         navigate(`/gastronomy/${res_id}/basket`);
     };
-
-    // Размножаем данные до 30 элементов
-    const expandedData: IDish[] = [];
-    for (let i = 0; i < 10; i++) {
-        mockGastronomyListData.forEach((dish) => {
-            expandedData.push({
-                ...dish,
-                id: i * mockGastronomyListData.length + dish.id,
-            });
-        });
-    }
 
     const handleDishClick = (dish: IDish) => {
         navigate(`/gastronomy/${res_id}/dish/${dish.id}`, {
@@ -33,10 +27,17 @@ export const GastronomyChooseDishesPage: React.FC = () => {
         });
     };
 
+    useEffect(() => {
+        if (!auth) return;
+        APIGetGastronomyDishes(auth.access_token, res_id)
+            .then((res) => setDishesList(res.data))
+            .catch((err) => console.error(err))
+    }, [auth, res_id]);
+
     return (
         <>
             <div className={css.items}>
-                {expandedData.map((dish) => (
+                {dishesList.map((dish) => (
                     <DishCard
                         key={dish.id}
                         {...dish}
