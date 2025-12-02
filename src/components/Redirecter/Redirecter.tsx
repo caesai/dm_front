8b26de60ae63909b330d5fbeb87c379e6d796f47
kurudoqqe <vadim.firedog@gmail.com>
2025-12-02 03:14:04 +0300

    change hook logic to local atom clear

--- src/components/Redirecter/Redirecter.tsx
+++ src/components/Redirecter/Redirecter.tsx
@@ -1,10 +1,8 @@
 import { useRedirectLogic } from '@/hooks/useRedirectLogic.ts';
 import { useNavigationHistory } from '@/hooks/useNavigationHistory.ts';
-import { useDishesListCleanup } from '@/hooks/useDishesListCleanup.ts';
 
 export const Redirecter = () => {
     useRedirectLogic();
     useNavigationHistory();
-    useDishesListCleanup();
     return null;
 };
