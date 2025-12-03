14e1666aac7b236b51d9e5457d138149d10b2d76
Vasily Kharlamov <caesai@yandex.ru>
2025-12-04 00:41:54 +0300

    Certificate Landing Fixing Rendering Logic And Proper Tests

--- jest.config.cjs
+++ jest.config.cjs
@@ -26,21 +26,26 @@ module.exports = {
   preset: "ts-jest",
   globals: {
     'ts-jest': {
-      tsconfig: './tsconfig.json',
+      tsconfig: './tsconfig.jest.json',
       useESM: true,
     }
   },
   moduleNameMapper: {
     "\\.(css|less|sass|scss)$": "identity-obj-proxy",
     "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/src/__mocks__/fileMock.js",
-    "^.+\\.svg$": "jest-transformer-svg",
+    // "^.+\\.svg$": "jest-transformer-svg",
     "^@/(.*)$": "<rootDir>/src/$1",
+    '^/img/(.*)$': '<rootDir>/public/img/$1',
     '^swiper/react$': '<rootDir>/src/__mocks__/swiper/react.js',
-    'swiper/css': '<rootDir>/src/__mocks__/styleMock.cjs',
-    'swiper/css/pagination': '<rootDir>/src/__mocks__/styleMock.cjs',
+    'swiper/css': '<rootDir>/src/__mocks__/styleMock.js',
+    '^swiper/css/bundle$': '<rootDir>/src/__mocks__/styleMock.js',
+    'swiper/css/pagination': '<rootDir>/src/__mocks__/styleMock.js',
     'swiper/react': '<rootDir>/src/__mocks__/swiper/react.js',
   },
   setupFilesAfterEnv: [
     "<rootDir>/setupTests.ts"
+  ],
+  transformIgnorePatterns: [
+    'node_modules/(?!(swiper|ssr-window|dom7)/)', // Include Swiper and its dependencies
   ],
 };
