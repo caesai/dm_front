8b6715b0da676c806349a1fec1c0ac875d2c99ff
Vasily Kharlamov <caesai@yandex.ru>
2025-12-09 22:03:31 +0300

    Build fix

--- src/__mocks__/certificates.mock.ts
+++ src/__mocks__/certificates.mock.ts
@@ -1,5 +1,5 @@
 import { ICertificate, ICertificateBlockProps } from '@/types/certificates.types.ts';
-import certificateImage from '/img/certificate_2.png';
+import certificateImage from '/img/certificate_new.png';
 
 export const certificatesListMock: ICertificate[] = [
     {
@@ -61,6 +61,6 @@ export const certificatesListMock: ICertificate[] = [
 export const mockCertificate: ICertificate = certificatesListMock[0];
 
 export const certificateBlock: ICertificateBlockProps = {
-    image: '/img/certificate_new.png',
+    image: certificateImage,
     description: 'Приятный подарок на любые праздники',
 };
