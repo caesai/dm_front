94fb450f5e5f98c9bf73a62a94b82a4d4aeca0c4
Vasily Kharlamov <caesai@yandex.ru>
2025-12-23 19:59:23 +0300

    .

--- src/pages/OnboardingPage/stages/StageOne.tsx
+++ src/pages/OnboardingPage/stages/StageOne.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import classNames from 'classnames';
 import { useNavigate } from 'react-router-dom';
 // Mocks
-import stage_logos from "/img/stage_logos.png";
+import stage_logos from '/img/stage_logos.png';
 // Styles
 import css from '@/pages/OnboardingPage/OnboardingPage.module.css';
 
@@ -13,32 +13,19 @@ export const StageOne: React.FC = () => {
             <div className={css.stage_page_wrapper}>
                 <div className={css.stage_footer}>
                     <div className={css.stage_description}>
-                        <h2 className={css.stage_description_title}>
-                            Выберите подходящий ресторан
-                        </h2>
+                        <h2 className={css.stage_description_title}>Выберите подходящий ресторан</h2>
                         <span className={css.stage_description_subtitle}>
                             Забронируйте стол, делитесь впечатлениями, участвуйте в мероприятиях
                         </span>
                     </div>
                     <div className={css.button_container}>
-                        <div
-                            className={css.redButton}
-                            onClick={() => navigate('/onboarding/2')}
-                        >
+                        <div className={css.redButton} onClick={() => navigate('/onboarding/2')}>
                             <span>Продолжить</span>
                         </div>
                     </div>
                 </div>
                 <div className={css.stageOne_wrapper}>
-                    <div className={css.stageOne_icons}>
-                        <img
-                            src={stage_logos}
-                            alt=""
-                            className={classNames(
-                                css.stageOne_icon,
-                            )}
-                        />
-                    </div>
+                    <img src={stage_logos} alt="" className={classNames(css.stageOne_icon)} />
                 </div>
             </div>
         </div>
