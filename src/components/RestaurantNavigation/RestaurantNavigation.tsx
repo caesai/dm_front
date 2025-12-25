467c596118279a7cfa17e67286db2483142f3518
Vasily Kharlamov <caesai@yandex.ru>
2025-12-25 09:31:34 +0300

    Maximum update depth exceeded Fix

--- src/components/RestaurantNavigation/RestaurantNavigation.tsx
+++ src/components/RestaurantNavigation/RestaurantNavigation.tsx
@@ -48,7 +48,13 @@ export const RestaurantNavigation: React.FC<IRestaurantNavigationProps> = ({
         return () => {
             document.removeEventListener('scroll', handleScroll);
         };
-    });
+    }, [hash, swiperInstance]);
+    useEffect(() => {
+        if (hash && swiperInstance) {
+            const index = hashes.indexOf(hash);
+            swiperInstance.slideTo(index);
+        }
+    }, [hash]);
 
     return (
         <div className={css.navigationSlider}>
