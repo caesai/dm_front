b4d764679e8e3b4999798a765aa25d1543cb7888
kurudoqqe <vadim.firedog@gmail.com>
2026-01-20 18:07:03 +0300

    change titles font styles

--- src/components/ContentBlock/HeaderContainer/HeaderContent/HeaderContainer.tsx
+++ src/components/ContentBlock/HeaderContainer/HeaderContent/HeaderContainer.tsx
@@ -6,11 +6,22 @@ interface IHeaderContentProps {
     title: React.ReactNode;
     id?: string;
     className?: string;
+    fontSize?: number;
 }
 
-export const HeaderContent: React.FC<IHeaderContentProps> = ({ title, id, className }): JSX.Element => {
+export const HeaderContent: React.FC<IHeaderContentProps> = (
+    {
+        title,
+        id,
+        className,
+        fontSize = 20
+    }): JSX.Element => {
     return (
-        <h3 className={classNames(css.contentHeader, className)} id={id}>
+        <h3
+            className={classNames(css.contentHeader, className)}
+            id={id}
+            style={{fontSize: `${fontSize}px`}}
+        >
             {title}
         </h3>
     );
