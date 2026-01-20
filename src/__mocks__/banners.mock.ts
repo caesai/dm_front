72a10bded49d3281187e9e9b916042bdb54fea81
kurudoqqe <vadim.firedog@gmail.com>
2026-01-20 18:09:07 +0300

    change mocked banner link

--- src/__mocks__/banners.mock.ts
+++ src/__mocks__/banners.mock.ts
@@ -4,14 +4,14 @@ import { IEventBanner } from '@/types/events.types.ts';
 export const BannersMock: IEventBanner[] = [
     {
         img_url: BannerImg,
-        link: '/events/497/details'
+        link: '/events/492/details'
     },
     {
         img_url: BannerImg,
-        link: '/events/497/details'
+        link: '/events/492/details'
     },
     {
         img_url: BannerImg,
-        link: '/events/497/details'
+        link: '/events/492/details'
     }
 ]
